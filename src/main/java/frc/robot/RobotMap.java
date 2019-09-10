fd3c13954d27a981fc8a997494d8461647854fe4
EB Driverstation <ryan.shavell@gmail.com>
2019-09-09 21:10:15 -0400

    First push

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+public class RobotMap {
+
+    public static final int LEFT_DRIVE_1 = 4;
+    public static final int LEFT_DRIVE_2 = 5;
+    public static final int LEFT_DRIVE_3 = 6;
+
+    public static final int RIGHT_DRIVE_1 = 1;
+    public static final int RIGHT_DRIVE_2 = 2;
+    public static final int RIGHT_DRIVE_3 = 3;
+}
