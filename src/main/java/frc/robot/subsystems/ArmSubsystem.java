2194003874e4a96ecfd481b3111eb4939bd9b22c
EB Driverstation <ryan.shavell@gmail.com>
2019-09-11 18:04:14 -0400

    Robot Map and Arm Subsystem

---
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,32 @@
+package frc.robot.subsystems;
+
+import com.explodingbacon.bcnlib.actuators.MotorGroup;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import frc.robot.RobotMap;
+
+public class ArmSubsystem {
+
+    public MotorGroup left, right;
+
+    public ArmSubsystem() {
+        left = new MotorGroup(
+                new CANSparkMax(RobotMap .LEFT_DRIVE_1, CANSparkMaxLowLevel.MotorType.kBrushless),
+                new CANSparkMax(RobotMap.LEFT_DRIVE_2, CANSparkMaxLowLevel.MotorType.kBrushless),
+                new CANSparkMax(RobotMap.LEFT_DRIVE_3, CANSparkMaxLowLevel.MotorType.kBrushless));
+
+        right = new MotorGroup(
+                new CANSparkMax(RobotMap.RIGHT_DRIVE_1, CANSparkMaxLowLevel.MotorType.kBrushless),
+                new CANSparkMax(RobotMap.RIGHT_DRIVE_2, CANSparkMaxLowLevel.MotorType.kBrushless),
+                new CANSparkMax(RobotMap.RIGHT_DRIVE_3, CANSparkMaxLowLevel.MotorType.kBrushless));
+        left.set(1);
+        right.set(1);
+    }
+
+    public void tankDrive(double leftPow, double rightPow) {
+        left.set(leftPow);
+        right.set(rightPow);
+    }
+
+
+}
